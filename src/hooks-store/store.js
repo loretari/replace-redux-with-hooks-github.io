deaae131cbc585ed8c75dd0af8c1609d7d8213ef
loretari <loretari@gmail.com>
2023-06-29 16:03:31 +0300

    init: creating a Concrete Store

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -5,17 +5,33 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
-const setState = useState(globalState)[1];
+export const useStore = () => {
+    const setState = useState(globalState)[1];
 
-useEffect(() => {
-    listeners.push(setState);
+    const dispatch = (actionIdentifier, payload) => {
+        const newState = actions[actionIdentifier](globalState, payload)
+        globalState = { ...globalState, ...newState};
 
-    return () => {
-        listeners = listeners.filter(li => li !== setState);
+        for (const listener of listeners) {
+            listener(globalState);
+        }
     }
+
+    useEffect(() => {
+        listeners.push(setState);
+
+        return () => {
+            listeners = listeners.filter(li => li !== setState);
+        }
     }, [setState]);
 
+    return [globalState, dispatch];
 
+};
 
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = {...globalState, ...initialState};
+    }
+    actions = {...actions, ...userActions};
 }
