e2ef742a48c24fb8286365eed740fd099e50e5e3
loretari <loretari@gmail.com>
2023-06-29 15:43:31 +0300

    init: finishing the Store Hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -5,8 +5,17 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
 const setState = useState(globalState)[1];
+
+const dispatch = actionIdentifier => {
+   const newState = actions[actionIdentifier](globalState)
+    globalState = { ...globalState, ...newState};
+
+   for (const listener of listeners) {
+       listener(globalState);
+   }
+}
 
 useEffect(() => {
     listeners.push(setState);
@@ -16,6 +25,13 @@ useEffect(() => {
     }
     }, [setState]);
 
+return [globalState, dispatch];
 
+};
 
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = {...globalState, ...initialState};
+    }
+    actions = {...actions, ...userActions};
 }
